aa2e58d0144af06bbf8d00a4e36aab25753be7c9
Adam F <geminiad@Gemini.(none)>
2014-04-09 00:15:43 -0700

    Added geocoding, so if you add a station with an address and no latitude and longitude, gmaps will attempt to find the lat and lon for you automatically.

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,8 @@
 class Station < ActiveRecord::Base
+  geocoded_by :street_address, :latitude  => :lat, :longitude => :lon
+  after_validation :geocode
+
+  def street_address
+    [address, city, state, country].compact.join(', ')
+  end
 end
