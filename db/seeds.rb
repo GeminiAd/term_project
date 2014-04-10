990124c61ddbc798299dbe12eecb8f84b897eeb8
Adam F <geminiad@Gemini.(none)>
2014-04-10 16:41:54 -0700

    Added seed data and Active Record Associations for the gas station model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+stations = Station.create([{ name: "Claremont Service Station", company: "Valero", address: "301 Claremont Boulevard",
+                             city: "San Francisco", state: "California", zip: 94127, country: "United States",
+                             url: "http://www.claremontservicestation.com/", phone: "(415) 664-5401",
+                             email: "claremontservicestation@gmail.com" },
+                           { name: "West Portal AUTO Services", company: "76", address: "800 Ulloa St.",
+                             city: "San Francisco", state: "California", zip: 94127, country: "United States",
+                             phone: "(415) 664-8563" },
+                           { name: "Ocean Avenue Service Station", address: "1490 Ocean Ave.", city: "San Francisco",
+                             state: "California", zip: 94112, country: "United States", url: "http://www.mycargirl.com/",
+                             phone: "(415) 586-0265" },
+                           { name: "Valero", company: "Valero", address: "1799 Ocean Avenue", city: "San Francisco",
+                             state: "California", zip: 94112, country: "United States", url: "http://www.valero.com",
+                             phone: "(415) 334-1353" },
+                           { name: "Sam Truong's Unocal 76", company: "76", address: "1855 Taraval Street",
+                             city: "San Francisco", state: "California", zip: 94116, country: "United States", 
+                             url: "http://www.76.com", phone: "(415) 753-5897" },
+                           { name: "Shell", company: "Shell", address: "2399 19th Avenue", city: "San Francisco",
+                             state: "California", zip: 94116, country: "United States" }])
