de56f19c65c644f01f5acc0eb30d0a2b172d6ad6
Adam Ferro <musicismytherapy@gmail.com>
2014-04-10 16:51:11 -0700

    Added seed data and Active Record Associations for the gas station model

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,5 +1,11 @@
 class Station < ActiveRecord::Base
+  has_many :prices
+  has_many :station_fuel_types
+  has_many :fuel_types, through: :station_fuel_types
+
   validates :name, presence: true, length: { minimum: 3 }
+  validates :address, :city, :state, :country, presence: true, if: "lat.nil? or lon.nil?"
+  #validates :lat, :lon, presence: true, if: "address.nil? or city.nil? or state.nil? or country.nil?"
 
   geocoded_by :street_address, :latitude  => :lat, :longitude => :lon
   after_validation :geocode
