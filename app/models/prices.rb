bcac490af5d6a5fff29ef86662c129113b8ba95e
Adam Ferro <musicismytherapy@gmail.com>
2014-04-11 02:19:32 -0700

    Added active record associations between the station_fuel_type and prices models, and associates between prices and stations and prices and fuel_types

--- app/models/prices.rb
+++ app/models/prices.rb
@@ -1,2 +1,6 @@
 class Prices < ActiveRecord::Base
+  belongs_to :station_fuel_types
+
+  has_one :station, through: :station_fuel_types
+  has_one :fuel_type, through: :station_fuel_types
 end
--- app/models/station_fuel_type.rb
+++ app/models/station_fuel_type.rb
@@ -1,4 +1,6 @@
 class StationFuelType < ActiveRecord::Base
   belongs_to :station
   belongs_to :fuel_type
+
+  has_many :prices
 end
