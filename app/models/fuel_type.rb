f55d53eb491b8ada3f58aa775e3006e2bd3631ba
Adam Ferro <musicismytherapy@gmail.com>
2014-04-11 01:31:50 -0700

    Added associations for the station_fuel_type and fuel_type models

--- app/models/fuel_type.rb
+++ app/models/fuel_type.rb
@@ -1,2 +1,4 @@
 class FuelType < ActiveRecord::Base
+  has_many :station_fuel_types
+  has_many :stations, through: :station_fuel_types
 end
--- app/models/station_fuel_type.rb
+++ app/models/station_fuel_type.rb
@@ -1,2 +1,4 @@
 class StationFuelType < ActiveRecord::Base
+  belongs_to :station
+  belongs_to :fuel_type
 end
