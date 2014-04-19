57012eb0fce1c9a1a601672e82de6a8fd12c0ee9
Adam Ferro <musicismytherapy@gmail.com>
2014-04-18 19:04:12 -0700

    Added the view and route to path '/stations/search'

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ TermProject::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   # post '/stations/:id/edit(.:format)' => 'stations#update'
+  get 'stations/search(.:format)' => 'welcome#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
