e30ee9fdfa60a5526b3c6f0fe470c564460455ca
Adam Ferro <musicismytherapy@gmail.com>
2014-04-22 16:17:15 -0700

    Undid the last change as it broke heroku's gmap app

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -32,7 +32,8 @@ class StationsController < ApplicationController
     params[:fuel_type_id] = 1
     ftid = params[:fuel_type_id]
 
-    @stations = Station.limit(10).includes(station_fuel_types: :price).where('station_fuel_types.fuel_type_id' => 1)
+    #@stations = Station.limit(10).includes(station_fuel_types: :price).where('station_fuel_types.fuel_type_id' => 1)
+    @stations = Station.take(10)    
 
     @stations.each { |station|
       station.station_fuel_types.each { |sft|
